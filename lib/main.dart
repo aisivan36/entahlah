9e88811f1c31a79abe0caa6a06ff6c07811f34b9
ivan-boginski <aisivan36@gmail.com>
2022-03-10 06:24:23 +0700

    enabled windows desktop

--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,10 @@ import 'package:get/get.dart';
 import 'package:koruwel/pages/sign/sign_in.dart';
 import 'package:koruwel/themes/theme.dart';
 
-void main()=>runApp(GetMaterialApp(home: const SignIn(),theme: Themes.dark,debugShowCheckedModeBanner: false,));
+void main() => runApp(
+      GetMaterialApp(
+        home: const SignIn(),
+        theme: Themes.dark,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
