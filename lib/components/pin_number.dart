ad8fb0f4a383012a9e45907e3bc23ea91248dbaa
ivan-boginski <aisivan36@gmail.com>
2022-03-10 06:56:59 +0700

    Functional Widget is not recommended, use stateless widget instead

--- lib/components/pin_number.dart
+++ lib/components/pin_number.dart
@@ -1,28 +1,47 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:koruwel/pages/sign/sign_in.dart';
-import 'package:koruwel/popup/popup_pin_created.dart';
 import 'package:koruwel/themes/text_style.dart';
 
-Widget pinNumber({String label='0', Function? onPressed, bool backSpace=false}){
-  return SizedBox(
-    width: 64,
-    height: 64,
-    child: OutlinedButton(
-      style: ElevatedButton.styleFrom(
-        side: const BorderSide(width: 1.0, color: Colors.white,),
-        primary: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(64),
+class PinNumber extends StatelessWidget {
+  const PinNumber(
+      {Key? key, this.onPressed, this.backSpace = false, this.label = '0'})
+      : super(key: key);
+
+  final String label;
+  final Function()? onPressed;
+  final bool backSpace;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 64,
+      height: 64,
+      child: OutlinedButton(
+        style: ElevatedButton.styleFrom(
+          side: const BorderSide(
+            width: 1.0,
+            color: Colors.white,
+          ),
+          primary: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(64),
+          ),
         ),
+        onPressed: () {
+          if (kDebugMode) {
+            print(onPressed);
+          }
+          if (onPressed != null) {
+            onPressed!();
+          }
+        },
+        child: backSpace
+            ? const Icon(Icons.backspace_outlined)
+            : Text(
+                label,
+                style: CustomTextStyle.formTitle,
+              ),
       ),
-      onPressed: (){
-        print(onPressed);
-        if(onPressed != null){
-          onPressed();
-        }
-      },
-      child: backSpace ? const Icon(Icons.backspace_outlined) : Text(label, style: CustomTextStyle.formTitle,),
-    ),
-  );
+    );
+  }
 }
