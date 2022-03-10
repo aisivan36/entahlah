4239451da2a35d2c9a46599f2f2b94040d6f787b
ivan-boginski <aisivan36@gmail.com>
2022-03-10 07:21:49 +0700

    Functional Widget is not recommended, use stateless widget instead

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:koruwel/themes/text_style.dart';
 
-Widget logo(){
-  return Row(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Expanded(child: Container(
-        margin: const EdgeInsets.only(top: 64),
-        padding: const EdgeInsets.all(16),
-        width: 128,
-        child: Column(
-          children: [
-            Container(
-              width: 128,
-              height: 128,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/icons/logo.png"),
-                      fit: BoxFit.cover
-                  )
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+            child: Container(
+          margin: const EdgeInsets.only(top: 64),
+          padding: const EdgeInsets.all(16),
+          width: 128,
+          child: Column(
+            children: [
+              Container(
+                width: 128,
+                height: 128,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/icons/logo.png"),
+                        fit: BoxFit.cover)),
               ),
-            ),
-            const Center(
-              child: Text("Koruwel",style: CustomTextStyle.heading,),
-            ),
-          ],
-        ),
-      ))
-    ],
-  );
+              const Center(
+                child: Text(
+                  "Koruwel",
+                  style: CustomTextStyle.heading,
+                ),
+              ),
+            ],
+          ),
+        ))
+      ],
+    );
+  }
 }
