ee3532098212967d9309d13a6efca5108cc86c01
ivan-boginski <aisivan36@gmail.com>
2022-03-10 07:09:23 +0700

    Functional Widget is not recommended, use stateless widget instead

--- lib/pages/pin/pin_confirm.dart
+++ lib/pages/pin/pin_confirm.dart
@@ -4,7 +4,7 @@ import 'package:koruwel/forms/form_pin_confirm.dart';
 import 'package:koruwel/forms/form_pin_create.dart';
 import 'package:koruwel/themes/text_style.dart';
 
-class PinConfirm extends StatelessWidget{
+class PinConfirm extends StatelessWidget {
   const PinConfirm({Key? key}) : super(key: key);
 
   @override
@@ -14,12 +14,30 @@ class PinConfirm extends StatelessWidget{
       backgroundColor: context.theme.backgroundColor,
       body: SingleChildScrollView(
         child: Column(
-          children:  [
-            const SizedBox(height: 96,),
-            const Center(child: Text("Confirm Your PIN", style: CustomTextStyle.formTitle,),),
-            const Center(child: Padding(padding: EdgeInsets.symmetric(horizontal: 32), child: Text("Security code is used to verify your every transaction to be more secure",textAlign: TextAlign.center, style: CustomTextStyle.paragraphGreyOut,),),),
-            const SizedBox(height: 32,),
-            formPinConfirm(context),
+          children: const [
+            SizedBox(
+              height: 96,
+            ),
+            Center(
+              child: Text(
+                "Confirm Your PIN",
+                style: CustomTextStyle.formTitle,
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  "Security code is used to verify your every transaction to be more secure",
+                  textAlign: TextAlign.center,
+                  style: CustomTextStyle.paragraphGreyOut,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            FromPinConfirm(),
           ],
         ),
       ),
